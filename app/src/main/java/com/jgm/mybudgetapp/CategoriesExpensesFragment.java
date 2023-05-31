9035c1f1bf02653f450b012b1edfca0a37270213
Juliana Miranda <jumiranda0501@gmail.com>
2023-05-31 17:39:55 -0300

    Add layout elements to fragments

--- app/src/main/java/com/jgm/mybudgetapp/CategoriesExpensesFragment.java
+++ app/src/main/java/com/jgm/mybudgetapp/CategoriesExpensesFragment.java
@@ -59,6 +59,6 @@ public class CategoriesExpensesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_categories_expenses, container, false);
+        return inflater.inflate(R.layout.fragment_categories_pager, container, false);
     }
 }
